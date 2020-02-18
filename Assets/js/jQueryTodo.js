d429653d26e4fda949a818467acacb20f8967ed8
Kerry Bennicke <kjbennicke@gmail.com>
2020-02-17 20:13:47 -0800

    Refactored JS and CSS.

--- Assets/js/jQueryTodo.js
+++ Assets/js/jQueryTodo.js
@@ -1,23 +1,4 @@
 //click the list items
-$("li").click(function ()
-{
-    //if li is gray
-    if ($(this).css("color") === "rgb(128, 128, 128)")
-    {
-        //Turns it black
-        $(this).css({
-            color: "black",
-            textDecoration: "none"
-        });
-    }
-    //else if li is black
-    else
-    {
-
-        $(this).css({
-            color: "gray",
-            textDecoration: "line-through"
-        });
-    }
-
+$("li").click(function (){
+    $(this).toggleClass("completed");
 });

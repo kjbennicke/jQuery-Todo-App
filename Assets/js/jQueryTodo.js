8b14a96b57aca10e7ec76ad771f533b96c1e0268
Kerry Bennicke <kjbennicke@gmail.com>
2020-02-19 22:01:07 -0800

    Ability to Add to the list, Mark Completed and Delete item added

--- Assets/js/jQueryTodo.js
+++ Assets/js/jQueryTodo.js
@@ -1,4 +1,20 @@
-//click the list items
-$("li").click(function (){
+//click the list items, strike through and color change
+$("ul").on("click", "li", function (){
     $(this).toggleClass("completed");
+});
+//Span delete Button functionality
+$("span").on("click", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+//Text input and adding Todo
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        var todoText = $(this).val();
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+        $(this).val(null);
+    }
+    
 });
